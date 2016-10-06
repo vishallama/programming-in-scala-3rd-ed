51382afb0dd340c03e42707da0fe588034d384ce
Vishal Lama <vishal.lama@gmail.com>
2016-10-06 09:56:52 -0600

    Add standalone object that uses ChecksumAccumulator

---
+++ src/chap04/Summer.scala
@@ -0,0 +1,10 @@
+package chap04
+
+import ChecksumAccumulator.calculate
+
+object Summer {
+  def main(args: Array[String]) = {
+    for (arg <- args)
+      println(arg + ": " + calculate(arg))
+  }
+}
