5dfa5343863b687f81f771c6b8d36f306b768e80
Vishal Lama <vishal.lama@gmail.com>
2016-10-18 06:54:24 -0600

    Add code showing constant patterns

--- src/chap15/Patterns.scala
+++ src/chap15/Patterns.scala
@@ -10,4 +10,13 @@ object Patterns {
     case BinOp(_, _, _) => println(expr + " is a binary operation")
     case _ => println("It's something else")
   }
+
+  // Constant patterns
+  def describe(x: Any) = x match {
+    case 5 => "five"
+    case true => "truth"
+    case "hello" => "hi!"
+    case Nil => "the empty list"
+    case _ => "something else"
+  }
 }
