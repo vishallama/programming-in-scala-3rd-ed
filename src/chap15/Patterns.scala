dde3c8ee66897cc96bd4ba0f694599bc1e0f8ba4
Vishal Lama <vishal.lama@gmail.com>
2016-10-18 07:08:54 -0600

    Add code showing variable patterns

--- src/chap15/Patterns.scala
+++ src/chap15/Patterns.scala
@@ -1,9 +1,12 @@
 package chap15
 
+import math.{E, Pi}
+
 object Patterns {
 
   val v = Var("x")
   val expr = BinOp("+", Number(1), v)
+  val expr1 = 1
 
   // Wild-card patterns
   expr match {
@@ -18,5 +21,17 @@ object Patterns {
     case "hello" => "hi!"
     case Nil => "the empty list"
     case _ => "something else"
+  }
+
+  // Variable patterns
+  expr1 match {
+    case 0 => "zero"
+    case somethingElse => "not zero: " + somethingElse
+  }
+
+  val pi = math.Pi
+  E match {
+    case `pi` => "strange math? Pi = " + pi
+    case _ => "OK"
   }
 }
