d3e2979b2a9b1cbdc0b4f517b1dbb261b99d9b8d
Vishal Lama <vishal.lama@gmail.com>
2016-10-19 11:53:12 -0600

    Add implementation of mergeSort function

--- src/chap16/lists.scala
+++ src/chap16/lists.scala
@@ -59,4 +59,36 @@ object lists extends App {
   }
 
   println(append(List(1, 2), List(3, 4)))
+
+  // Merge-sort
+  def mergeSort[T](less: (T, T) => Boolean)
+    (xs: List[T]): List[T] = {
+
+    def merge(xs: List[T], ys: List[T]): List[T] =
+      (xs, ys) match {
+      case (Nil, _) => ys
+      case (_, Nil) => xs
+      case (x :: xs1, y :: ys1) =>
+        if (less(x, y)) x :: merge(xs1, ys)
+        else y :: merge(xs, ys1)
+    }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(mergeSort(less)(ys), mergeSort(less)(zs))
+    }
+  }
+
+  println(mergeSort((x: Int, y: Int) => x < y) (List(5, 7, 0, 10, 3)))
+
+  // Use of currying to specialize the mergeSort function for particular
+  // comparison functions.
+  val intSort = mergeSort((x: Int, y: Int) => x < y) _
+  val reverseIntSort = mergeSort((x: Int, y: Int) => x > y) _
+  val mixedInts = List(10, 0, 5, 3, 8, 4, 1)
+
+  println(intSort(mixedInts))
+  println(reverseIntSort(mixedInts))
 }
