479ba4d0e103e83d81e3e252de9185d7214faea4
Vishal Lama <vishal.lama@gmail.com>
2016-10-21 08:17:22 -0600

    Add factory method to Queue

--- src/chap19/Queue.scala
+++ src/chap19/Queue.scala
@@ -1,13 +1,15 @@
 package chap19
 
-class Queue[T](
+class Queue[T] private (
     private val leading: List[T],
     private val trailing: List[T]
     ) {
 
+  def this(elems: T*) = this(elems.toList, Nil)
+
   private def mirror =
     if (leading.isEmpty)
-      new Queue(trailing.reverse, Nil)
+      Queue(trailing.reverse, Nil)
     else
       this
 
@@ -15,9 +17,14 @@ class Queue[T](
 
   def tail = {
       val q = mirror
-      new Queue(q.leading.tail, q.trailing)
+      Queue(q.leading.tail, q.trailing)
   }
 
   def enqueue(x: T) =
-    new Queue(leading, x :: trailing)
+    Queue(leading, x :: trailing)
+}
+
+object Queue {
+  // constructs a queue with initial elements 'xs'
+  def apply[T](xs: T*) = new Queue[T](xs.toList, Nil)
 }
