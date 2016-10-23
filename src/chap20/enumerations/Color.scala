dccdb3e302fdbf1b8a210d3b9031ac8adee34e68
Vishal Lama <vishal.lama@gmail.com>
2016-10-22 21:33:24 -0600

    Define Color class using Enumeration

---
+++ src/chap20/enumerations/Color.scala
@@ -0,0 +1,5 @@
+package chap20.enumerations
+
+object Color extends Enumeration {
+  val Red, Green, Blue = Value
+}
---
+++ src/chap20/enumerations/ColorTest.scala
@@ -0,0 +1,10 @@
+package chap20.enumerations
+
+import Color._
+
+object ColorTest extends App {
+
+  println(Red)      // Red
+  println(Red.id)   // 0
+  println(Color)    // Color
+}
