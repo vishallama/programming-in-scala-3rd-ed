037db3693fcddd7cabe782a2b81e40f577c9fef4
Vishal Lama <vishal.lama@gmail.com>
2016-10-22 20:51:32 -0600

    Change type of bessy object

--- src/chap20/abstract_types/AnimalTest.scala
+++ src/chap20/abstract_types/AnimalTest.scala
@@ -2,8 +2,11 @@ package chap20.abstract_types
 
 object AnimalTest extends App {
 
-  val bessy: Animal = new Cow
+  val bessy = new Cow
   // The following won't compile, because bessy can only eat food
   // of type Grass.
   // bessy eat (new Fish)
+
+  // This compiles just fine
+  bessy eat (new Grass)
 }
