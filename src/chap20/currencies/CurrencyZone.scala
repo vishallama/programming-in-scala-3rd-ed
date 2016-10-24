c644a0b28333765f90136f9d9e22011b17422c37
Vishal Lama <vishal.lama@gmail.com>
2016-10-24 11:51:29 -0600

    Add a few more methods to AbstractCurrency

--- src/chap20/currencies/CurrencyZone.scala
+++ src/chap20/currencies/CurrencyZone.scala
@@ -3,21 +3,29 @@ package chap20.currencies
 abstract class CurrencyZone {
 
   type Currency <: AbstractCurrency
-
   def make(x: Long): Currency
 
   abstract class AbstractCurrency {
+
     val amount: Long
-
     def designation: String
-
-    override def toString = amount + " " + designation
 
     def + (that: Currency): Currency =
       make(this.amount + that.amount)
 
     def * (x: Double): Currency =
       make((this.amount * x).toLong)
+
+    def - (that: Currency): Currency =
+      make(this.amount - that.amount)
+
+    def / (that: Double) =
+      make((this.amount / that).toLong)
+
+    def / (that: Currency) =
+      this.amount.toDouble / that.amount
+
+    override def toString = amount + " " + designation
   }
 
   val CurrencyUnit: Currency
